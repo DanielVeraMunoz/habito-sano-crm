81f80fbff57e6c922c7f0dbb04e2e5df68ff33b8
Daniel Vera <dani.vmg91@gmail.com>
2026-09-07 21:12:38 +0200

    feat: add PatientFactory fake data

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Patient;
+use App\Models\Staff;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,16 @@ class PatientFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->firstName(),
+            'surname' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'assigned_to' => Staff::factory(),
+            'plan' => $this->faker->randomElement(['suelta', 'mensual', 'trimestral', 'semestral', 'anual']),
+            'sessions_remaining' => $this->faker->numberBetween(0, 10),
+            'status' => $this->faker->randomElement(['activo', 'pausado', 'inactivo']),
+            'notes' => $this->faker->optional()->paragraph(),
+            'diet_plan' => $this->faker->optional()->paragraph(),
         ];
     }
 }
