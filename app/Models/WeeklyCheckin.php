2ad8d6bc2db959e0e7508ba2f17776dda6e334f5
Daniel Vera <dani.vmg91@gmail.com>
2026-09-09 20:40:18 +0200

    feat: create with eloquent model, factory and migration

---
+++ app/Models/WeeklyCheckin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WeeklyCheckin extends Model
+{
+    /** @use HasFactory<\Database\Factories\WeeklyCheckinFactory> */
+    use HasFactory;
+}
